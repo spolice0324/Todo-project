d2909fba5bccfb2f6f711a4e441c68c08855ce35
yeokyoung song <spolice0324@gmail.com>
2023-08-03 15:08:31 +0900

    style : ToDoApp 부분 스타일링 적용

--- todo/src/components/ToDoApp.js
+++ todo/src/components/ToDoApp.js
@@ -1,11 +1,13 @@
 function ToDoApp() {
   return (
-    <section className="todo-app">
-      <header className="header">
-        <h1>todos</h1>
+    <section className="todo-app bg-#fff my-40 mx-130 relative text-base leading-5 text-gray-700 min-w-min max-w-lg mx-auto p-4">
+      <header className="todo-header block shadow-xl">
+        <h1 className="todo-todos absolute block -top-[155px] w-full text-7xl  font-thin text-center text-red-200">
+          todos
+        </h1>
         <input
-          className="new-todo"
-          placeholder="할 일을 입력해주세요."
+          className="new-todo w-full relative pl-[60px] py-[16px] border-none bg-black bg-opacity-0 shadow-xl italic text-2xl text-thin text-gray-200"
+          placeholder="What needs to be done?"
           autoFocus
         />
       </header>
