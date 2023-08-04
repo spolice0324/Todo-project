e92b93205b01174a94712aa4b9541ccedfae7dc7
yeokyoung song <spolice0324@gmail.com>
2023-08-04 15:28:19 +0900

    fix : header 컴포넌트 분리

--- todo/src/components/ToDoApp.js
+++ todo/src/components/ToDoApp.js
@@ -1,19 +1,11 @@
-import ToDoFooter from "./ToDoFooter";
 import ToDoMain from "./ToDoMain";
+import ToDoFooter from "./ToDoFooter";
+import ToDoHeader from "./ToDoHeader";
 
 function ToDoApp() {
   return (
     <section className="todo-app bg-#fff my-40 mx-130 relative text-base leading-5 text-gray-700 min-w-min max-w-lg mx-auto p-4">
-      <header className="todo-header block shadow-xl">
-        <h1 className="todo-todos absolute block -top-[155px] w-full text-7xl  font-thin text-center text-red-200">
-          todos
-        </h1>
-        <input
-          className="new-todo w-full relative pl-[60px] py-[16px] border-none bg-black bg-opacity-0 shadow-xl italic text-2xl font-thin text-gray-200"
-          placeholder="What needs to be done?"
-          autoFocus
-        />
-      </header>
+      <ToDoHeader />
       <ToDoMain />
       <ToDoFooter />
     </section>
