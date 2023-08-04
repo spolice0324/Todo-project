698a0fc0ce9d2db4e13b30f3b06fad985838e310
yeokyoung song <spolice0324@gmail.com>
2023-08-04 15:27:52 +0900

    style : footer에 스타일 적용, isActive 속성으로 필터링 스타일링 적용

--- todo/src/components/ToDoFooter.js
+++ todo/src/components/ToDoFooter.js
@@ -1,30 +1,46 @@
 import { NavLink } from "react-router-dom";
 
 export default function ToDoFooter() {
+  const unSelectedStyle = " inline-block pl-3 rounded-sm ";
+  const selectedStyle = "   rounded-sm border border-solid border-red-200 p-1 ";
+
   return (
     <>
-      <footer className="footer">
-        <span className="todo-count">
-          <strong>0</strong> items left
+      <footer className="footer block footer py-[10px]  h-[20px] text-center border-t border-gray-300 font-thin">
+        <span className="todo-count abosolute float-left text-left text-gray-500 font-thin">
+          <strong className="todo-num  font-thin">0</strong> items left
         </span>
-        <ul className="filters">
-          <li>
-            <NavLink exact={true} to="/" activeClassName="selected">
+        <ul className="filters inline-block right-0 left-0 font-thin p-0.5">
+          <li className={unSelectedStyle}>
+            <NavLink
+              exact={"true"}
+              to="/"
+              className={({ isActive }) => (isActive ? selectedStyle : "")}
+            >
               All
             </NavLink>
           </li>
-          <li>
-            <NavLink to="/active" activeClassName="selected">
+          <li className={unSelectedStyle}>
+            <NavLink
+              to="/active"
+              className={({ isActive }) => (isActive ? selectedStyle : "")}
+            >
               Active
             </NavLink>
           </li>
-          <li>
-            <NavLink to="/completed" activeClassName="selected">
+          <li className={unSelectedStyle}>
+            <NavLink
+              to="/completed
+            "
+              className={({ isActive }) => (isActive ? selectedStyle : "")}
+            >
               Completed
             </NavLink>
           </li>
         </ul>
-        <button className="clear-completed">Clear completed</button>
+        <button className="clear-complete relative cursor-pointer float-right ">
+          Clear completed
+        </button>
       </footer>
     </>
   );
